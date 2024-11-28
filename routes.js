744d82893fb137353ea9dad6aaffa57ef710c8f4
Bruno Silva <84159328+brunocsilva41@users.noreply.github.com>
2024-11-28 19:21:05 -0300

    corrigindo edição do usuario

--- routes.js
+++ routes.js
@@ -134,7 +134,9 @@ router.put('/usuarios/:id', [
         if (result.affectedRows === 0) {
             return res.status(404).json({ message: 'Usuário não encontrado' });
         }
-        res.status(200).json({ message: 'Usuário atualizado com sucesso!' });
+
+        const [updatedUser] = await db.promise().query('SELECT Id, nome, email, role, endereco, telefone_usuario FROM usuarios WHERE Id = ?', [id]);
+        res.status(200).json({ message: 'Usuário atualizado com sucesso!', user: updatedUser[0] });
     } catch (err) {
         res.status(500).json({ message: 'Erro no servidor' });
     }
